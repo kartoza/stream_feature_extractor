a3ab5d8b325eeb8bd81ed9ef52226d8d10254ed0
Tim Sutton <tim@linfiniti.com>
2014-05-07 15:25:00 +0200

    Initial commit of plugin framework

---
+++ __init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ StreamFeatureTool
+                                 A QGIS plugin
+ A tool to extract features from a stream network.
+                             -------------------
+        begin                : 2014-05-07
+        copyright            : (C) 2014 by Linfiniti Consulting CC.
+        email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+def classFactory(iface):
+    # load StreamFeatureTool class from file StreamFeatureTool
+    from stream_feature_extractor import StreamFeatureTool
+    return StreamFeatureTool(iface)
---
+++ stream_feature_extractor_dialog.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ StreamFeatureToolDialog
+                                 A QGIS plugin
+ A tool to extract features from a stream network.
+                             -------------------
+        begin                : 2014-05-07
+        copyright            : (C) 2014 by Linfiniti Consulting CC.
+        email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'stream_feature_extractor_dialog_base.ui'))
+
+
+class StreamFeatureToolDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(StreamFeatureToolDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
