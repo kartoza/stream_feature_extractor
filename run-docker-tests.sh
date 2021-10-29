c5c65dd7e026df748537ecb138a8cfd52d2fd7ba
Samweli <samweli@kartoza.com>
2021-10-29 14:50:07 +0300

    updated github actions to run test when PR is reopened and updated local docker tests

--- run-docker-tests.sh
+++ run-docker-tests.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
 
-QGIS_IMAGE_V_3_16=samtwesa/qgis-testing-environment-docker:release-3_16
+QGIS_IMAGE=qgis/qgis
 
-IMAGES=($QGIS_IMAGE_V_3_16)
+QGIS_IMAGE_V_3_16=release-3_16
+QGIS_IMAGE_V_3_20=release-3_20
 
+QGIS_VERSION_TAGS=($QGIS_IMAGE_V_3_16 $QGIS_IMAGE_V_3_20)
 
-for IMAGE in "${IMAGES[@]}"
+export IMAGE=$QGIS_IMAGE
+
+for TAG in "${QGIS_VERSION_TAGS[@]}"
 do
-    echo "Running tests for $IMAGE"
-    if [[ $1 -eq "--remote" ]]; then
-      xhost +
-    fi
+    echo "Running tests for QGIS $TAG"
+    export QGIS_VERSION_TAG=$TAG
+    export WITH_PYTHON_PEP=false
+    export ON_TRAVIS=false
+    export MUTE_LOGS=true
 
-    docker run -d --name qgis-testing-environment \
-    -v ${PWD}:/tests_directory \
-    -e WITH_PYTHON_PEP=false \
-    -e ON_TRAVIS=false \
-    -e MUTE_LOGS=true \
-    -v /tmp/.X11-unix:/tmp/.X11-unix  \
-    -e DISPLAY=:99 \
-    ${IMAGE}
+    docker-compose up -d
 
     sleep 10
 
-    docker exec -it qgis-testing-environment sh -c "qgis_setup.sh stream_feature_extractor"
-
-    # FIX default installation because the sources must be in "stream_feature_extractor" parent folder
-    docker exec -it qgis-testing-environment sh -c "rm -f  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/stream_feature_extractor"
-    docker exec -it qgis-testing-environment sh -c "ln -s /tests_directory/ /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/stream_feature_extractor"
-
-    # Run the real test
-    time docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh test_suite.test_package"
-
-    docker stop qgis-testing-environment
-    docker rm qgis-testing-environment
+    docker-compose exec -T qgis-testing-environment qgis_testrunner.sh test_suite.test_package
+    docker-compose down
 
 done
